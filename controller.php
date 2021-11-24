0983464e28f8f85eefc16bee42ad8945531b11ba
Uriel Badillo Garrido <92193001+UrieLBadilloG@users.noreply.github.com>
2021-11-23 22:42:57 -0500

    new

--- controller.php
+++ controller.php
@@ -8,7 +8,7 @@ $model->clave=$_POST["txtClave"];
 
 $filaController=$model->Logear();
 if($filaController>0){
-    echo"<h1>Bienvenido Usuario</h1>";
+    header("Location: inicio.html");
 }else{
     echo "<h1>Usuario o contraseña Incorrectos!</h1>";
     header("refresh:2; url=http://localhost/login/"); 
