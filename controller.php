9a609aba4aca0cbf94da5444fada5995a19aaf75
Uriel Badillo Garrido <92193001+UrieLBadilloG@users.noreply.github.com>
2021-11-23 22:15:46 -0500

    1

--- controller.php
+++ controller.php
@@ -1,36 +1,16 @@
 <?php
-include("conexion.php");
+
 require_once'model.php';
-//instancia
+//instalcia
 $model= new Model();
 $model->usuario=$_POST['txtUsuario'];
-$usuario = $_POST['txtUsuario'];;
 $model->clave=$_POST["txtClave"];
-$rango = 0;
 
 $filaController=$model->Logear();
 if($filaController>0){
-
-   $sql = "SELECT Rango FROM USUARIO WHERE ID_usuario = $usuario";
-   $stmt = sqlsrv_query( $con, $sql );
-   while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-      $rango =  $row['Rango'];
-   }
-   echo"<h1>Bienvenido Usuario con rango $rango</h1>";
-   switch($rango){
-      case 1:
-         header("Location: admin-perfil-crear.php?");
-         break;
-     case 2:
-         header("Location: admin-perfil-crear.php?");
-         break;
-     case 3:
-         header("Location: user-perfil.php?");
-         break;
-   }
-   
-
+    echo"<h1>Bienvenido Usuario</h1>";
 }else{
     echo "<h1>Usuario o contraseña Incorrectos!</h1>";
+    header("refresh:2; url=http://localhost/login/"); 
 }
 ?>
--- model.php
+++ model.php
@@ -5,19 +5,18 @@ class Model
     var $id;
     var $usuario;
     var $clave;
-    var $con;
     function __construct(){
 
     }
     function Logear(){
-        $cadenaCnx="sqlsrv:Server=DESKTOP-H3MGCA8;Database=AM";
+        $cadenaCnx="sqlsrv:Server=DESKTOP-H3MGCA8;Database=UPIITRAVEL";
         $user="Uri";
         $pass="123456";
         $cnx= new PDO($cadenaCnx, $user, $pass);
         
         try{
             $consulta=$cnx->prepare("SELECT *FROM USUARIO  WHERE
-            ID_usuario=:parametro1 AND contraseña=:parametro2");
+            Correo=:parametro1 AND Contraseña=(SELECT dbo.fun_encriptar(:parametro2))");
             $consulta->bindValue(":parametro1", $this->usuario);
             $consulta->bindValue(":parametro2", $this->clave);
             $consulta->execute();
